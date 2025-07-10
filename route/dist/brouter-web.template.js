0c9848ac3f36085d90b7f519b38874dbff6609a4
wbjon <45140488+wbjon@users.noreply.github.com>
2025-07-11 07:07:19 +0800

    0711

--- route/dist/brouter-web.template.js
+++ route/dist/brouter-web.template.js
@@ -22,9 +22,7 @@ iconColor:"white",
 viewBox:"0 0 32 52"},
 r=(function(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function, not "+typeof t);e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,enumerable:!1,writable:!0,configurable:!0}}),t&&(Object.setPrototypeOf?Object.setPrototypeOf(e,t):e.__proto__=t)}(c,a.default.Icon),r(c,[{key:"createIcon",value:function(e){
 var t=e&&"DIV"===e.tagName?e:document.createElement("div"),o=this.options,e=o.map_pin||"M16,1 C7.7146,1 1,7.65636364 1,15.8648485 C1,24.0760606 16,51 16,51 C16,51 31,24.0760606 31,15.8648485 C31,7.65636364 24.2815,1 16,1 L16,1 Z";
-
-return t.innerHTML=
-'<svg width="'+o.iconSize[0]+'px" height="'+o.iconSize[1]+'px" viewBox="'+o.viewBox+'" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="'+e+'" fill="'+o.markerColor+'"></path></svg>',
+return t.innerHTML='<svg width="'+o.iconSize[0]+'px" height="'+o.iconSize[1]+'px" viewBox="'+o.viewBox+'" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="'+e+'" fill="'+o.markerColor+'"></path></svg>',
 o.icon&&t.appendChild(this._createInner()),o.className+=0<o.className.length?" "+o.extraDivClasses:o.extraDivClasses,this._setIconStyles(t,"icon"),this._setIconStyles(t,"icon-"+o.markerColor),t}},{key:"_createInner",value:function(){
 var e=document.createElement("i"),t=this.options;
 
