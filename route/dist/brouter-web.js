813cbe6101ca59b873fe1d44a8070c8eb19f37c3
wbjon <45140488+wbjon@users.noreply.github.com>
2025-07-11 16:52:56 +0800

    0711

--- route/dist/brouter-web.js
+++ route/dist/brouter-web.js
@@ -22,7 +22,7 @@ iconColor:"yellow",
 viewBox:"0 0 32 52"},
 r=(function(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function, not "+typeof t);e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,enumerable:!1,writable:!0,configurable:!0}}),t&&(Object.setPrototypeOf?Object.setPrototypeOf(e,t):e.__proto__=t)}(c,a.default.Icon),r(c,[{key:"createIcon",value:function(e){
 var t=e&&"DIV"===e.tagName?e:document.createElement("div"),o=this.options,e=o.map_pin||"M16,1 C7.7146,1 1,7.65636364 1,15.8648485 C1,24.0760606 16,51 16,51 C16,51 31,24.0760606 31,15.8648485 C31,7.65636364 24.2815,1 16,1 L16,1 Z";
-return window.starHTML='<svg width="'+o.iconSize[0]+'px" height="'+o.iconSize[1]+'px" viewBox="'+o.viewBox+'" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="'+e+'" fill="'+o.markerColor+'"></path></svg>',t.innerHTML='',
+return window.starHTML=`<svg width="${o.iconSize[0]}px" height="${o.iconSize[1]}px" viewBox="${o.viewBox}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="${e}" fill="${o.markerColor}"></path></svg>`,t.innerHTML='',
 o.icon&&t.appendChild(this._createInner()),o.className+=0<o.className.length?" "+o.extraDivClasses:o.extraDivClasses,this._setIconStyles(t,"icon"),this._setIconStyles(t,"icon-"+o.markerColor),t}},{key:"_createInner",value:function(){
 var e=document.createElement("i"),t=this.options;
 if(t.icon=="star"){const emoji=document.createElement("span");emoji.textContent=testThis.split(';')[1]||"⭐";emoji.style.fontSize="20px";return emoji}
