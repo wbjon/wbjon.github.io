ab23f8c8325989bb9f408aca397dd9c3bb8704e7
wbjon <45140488+wbjon@users.noreply.github.com>
2026-04-27 19:34:32 +0800

    2026.04.27

--- route/dist/brouter-web.js
+++ route/dist/brouter-web.js
@@ -128,7 +128,32 @@ function(e,o){
    const marker=L.circleMarker([lat,lng],{radius:4,color:'blue',weight:1,fill:false,interactive:false});marker.bindTooltip(feature.properties?.name||"POI",{permanent:true,direction:'top'})
    group.addLayer(marker)
   }
- });testLayer=trackLayer
+ })
+ trackLayer.eachLayer(layer=>{
+  let latlngs=layer.getLatLngs()
+  let total=0,nextKm=1
+  for(let i=1;i<latlngs.length;i++){
+   const p1=latlngs[i-1]
+   const p2=latlngs[i]
+   const d=t.distance(p1,p2)
+   total+=d
+   while(total>=nextKm*1000){
+    const ratio=(nextKm*1000-(total-d))/d
+    const lat=p1.lat+(p2.lat-p1.lat)*ratio
+    const lng=p1.lng+(p2.lng-p1.lng)*ratio
+    const kmMarker=L.marker([lat,lng],{
+     icon:L.divIcon({
+      html:`<div style="
+       background:white;
+       border:1px solid black;
+       border-radius:4px;
+       padding:2px 4px;
+       font-size:11px;">${nextKm}</div>`,className:''}),interactive:false})
+    group.addLayer(kmMarker)
+    nextKm++
+   }
+  }
+ })
  return group
 },
 layerOptions:BR.Track.getGeoJsonOptions(a),addToMap:!1,fileSizeLimit:BR.conf.trackSizeLimit||10240,shortcut:{open:79}},_initContainer:function(){var o,e=this.loader,t=L.DomUtil.get("navbarLoadTracksContainer");L.DomEvent.addListener(document,"keydown",this._keydownListener,this),(o=L.DomUtil.create("input","hidden",t)).type="file",o.multiple="multiple",this.options.formats?o.accept=this.options.formats.join(","):o.accept=".gpx,.kml,.json,.geojson",o.style.display="none",o.addEventListener("change",function(){e.loadMultiple(this.files),this.value=""},!1);t=L.DomUtil.get("navbarLoadTracks");L.DomEvent.disableClickPropagation(t),L.DomEvent.on(t,"click",function(e){o.click(),e.preventDefault()});t=L.DomUtil.create("div");return t.hidden=!0,t},_keydownListener:function(e){BR.Util.keyboardShortcutsAllowed(e)&&e.keyCode===this.options.shortcut.open&&!1===e.shiftKey&&$("#navbarLoadTracks")[0].click()}}),t.createPane("tracks"),t.getPane("tracks").style.zIndex=350;var o=new TracksLoader;return o.addTo(t),
