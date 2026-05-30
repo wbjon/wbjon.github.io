3c417870371eab123c75ac26362914be53195f61
wbjon <45140488+wbjon@users.noreply.github.com>
2026-05-30 08:06:05 +0800

    2026.05.30

--- route/dist/brouter-web.js
+++ route/dist/brouter-web.js
@@ -71,7 +71,7 @@ _waypointClickHandler:function(t){
  }else this.removeWaypoint(t.marker,function(){})},
 addWaypoint:function(t,e,n,i,o){const beeline=e//t:marker,e:beeline,n:prev,i:next,o:cb
 t instanceof L.LatLng&&(t=new L.Marker(t,{title:this.options.tooltips.waypoint,draggable:!0})),t._routing={prevMarker:n,nextMarker:i,prevLine:null,nextLine:null,timeoutID:null,beeline:e||!1};
-if(activeRange[1]===n)activeRange[1]=t//改
+if(activeRange[1]===n){activeRange[1]=t;t.beeline=true}//改
 var r=this.options.icons;n?i?t.setIcon(r.normal):t.setIcon(r.end):t.setIcon(r.start);
 var s=i&&this.getFirst()&&i._leaflet_id===this.getFirst()._leaflet_id,a=n&&this.getFirst()&&n._leaflet_id===this.getFirst()._leaflet_id,e=n&&this.getLast()&&n._leaflet_id===this.getLast()._leaflet_id;s&&i.setIcon(r.normal),e&&!a&&n.setIcon(r.normal),null===this.getFirst()&&null===this.getLast()?(this._waypoints._first=t,this._waypoints._last=t):null===i?this._waypoints._last=t:null===n&&(this._waypoints._first=t),null!==t._routing.prevMarker&&((t._routing.prevMarker._routing.nextMarker=t)._routing.prevLine=t._routing.prevMarker._routing.nextLine,null!==t._routing.prevLine&&(t._routing.prevLine._routing.nextMarker=t)),null!==t._routing.nextMarker&&((t._routing.nextMarker._routing.prevMarker=t).nextLine=t._routing.nextMarker._routing.prevLine,null!==t._routing.nextLine&&(t._routing.nextLine._routing.prevMarker=t)),t.on("mouseover",this._fireWaypointEvent,this),t.on("mouseout",this._fireWaypointEvent,this),
 t.on("dragstart",this._fireWaypointEvent,this),
