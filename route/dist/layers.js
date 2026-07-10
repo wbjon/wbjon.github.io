c488f68b666511c4b7563c51ce78d8411532c995
wbjon <45140488+wbjon@users.noreply.github.com>
2026-07-11 07:33:36 +0800

    0711

--- route/dist/layers.js
+++ route/dist/layers.js
@@ -68,7 +68,7 @@ const createGeoJsonLayer=function(url,options){
      else Name=item.ExitName.Zh_tw,Des=item.LocationDescription,Lon=item.ExitPosition.PositionLon,Lat=item.ExitPosition.PositionLat
      if(url.includes('TYMC')||url.includes('KRTC'))Name=item.StationName.Zh_tw+Name//TYMC:桃捷、KRTC:高捷
      Name=Name.replace(/\s+/g,"");Des=Des?Des.replace(/\s+/g,""):'空'
-     if(Des.includes(';'))console.log(Name,Des)
+     Des.split(";")[0]//KLRT高雄輕軌站的Des會有; 需刪除後方的英文地址
      geojson.features.push(
       {
        type:"Feature",
