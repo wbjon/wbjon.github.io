a4f168a2c252158c97e14b68c21a56e4b2e1b87c
wbjon <45140488+wbjon@users.noreply.github.com>
2026-07-10 08:50:22 +0800

    0710

--- route/dist/brouter-web.js
+++ route/dist/brouter-web.js
@@ -141,16 +141,18 @@ function _slicedToArray(e,r){return _arrayWithHoles(e)||_iterableToArrayLimit(e,
 _replaceMvtTileKey:function(e){if(e)for(var r=0,a=Object.values(e.sources);r<a.length;r++){var t,n=a[r];if(!n.url||(t=this.getKeyName(n.url))&&BR.keys[t.name]&&(n.url=n.url.replace("{".concat(t.urlVar,"}"),BR.keys[t.name])),n.tiles){var o=n.tiles,s=_createForOfIteratorHelper(null==o?void 0:o.entries());try{for(s.s();!(u=s.n()).done;){var i=_slicedToArray(u.value,2),l=i[0],u=i[1],i=this.getKeyName(u);i&&BR.keys[i.name]&&(o[l]=u.replace("{".concat(i.urlVar,"}"),BR.keys[i.name]))}}catch(e){s.e(e)}finally{s.f()}}}},
 createGeoJsonLayer:function(url,options){
  options.pointToLayer=function(feature,latlng){
-  return L.marker(latlng,{
+ return L.circleMarker(latlng,{radius:6,weight:1,fillOpacity:0.8})
+ .bindPopup(feature.properties.name)
+  /*return L.marker(latlng,{
                           icon:L.icon({
                           iconUrl:"/單車站2.png",
                           iconSize:[24,32],
                           iconAnchor:[12,16]
                           })
-                         }).bindPopup(feature.properties.name)
-                           .on("popupopen",function(e){
-                                     e.popup.setContent(`Hello World`)
-                                    })
+                         }).bindPopup(feature.properties.name)*/
+                          //  .on("popupopen",function(e){
+                          //            e.popup.setContent(`Hello World`)
+                          //           })
  }
  var layer=L.geoJSON(null,options)
  fetch(url).then(res=>res.json()).then(obj=>{
